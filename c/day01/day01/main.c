1406bd0fcbf80b5b1645a08af4b7f893ca56fd8c
kelseyduffy <kduffy@osisoft.com>
2022-11-30 21:04:29 -0500

    prepping for day 1

---
+++ c/day01/day01/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  day01
+//
+//  Created by Kelsey Duffy on 11/30/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ python/01.py
@@ -0,0 +1,5 @@
+nums = []
+
+with open('python/01.in','r') as f:
+    for x in f.readlines():
+        nums.append(int(x))
