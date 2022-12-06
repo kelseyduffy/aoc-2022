49c545b4cf43713d879ce0e158ad51a7be59ce08
Kelsey Duffy <kduffy@osisoft.com>
2022-12-06 13:17:25 -0500

    Update 06.py

--- python/06.py
+++ python/06.py
@@ -4,7 +4,7 @@ with open('python/06.in','r') as f:
 
 for count in [4,14]:
     for i in range(len(datastream)):
-        four_signals = {signal for signal in datastream[i:i+count]}
-        if len(four_signals) == count:
+        unique_signals = {signal for signal in datastream[i:i+count]}
+        if len(unique_signals) == count:
             print(f'{count}: {i + count}')
             break
