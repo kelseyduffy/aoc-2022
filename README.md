9f1f0a347aab1f28a1b113258f9104bf4ceb573a
kelseyduffy <kduffy@osisoft.com>
2022-12-08 09:33:15 -0500

    python day08

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ Whatever language I feel like that day. Probably mostly Python and C, maybe some
 |[05](https://adventofcode.com/2022/day/5)|stacks|51811|50089|[python](python/05.py)|--|--|crate stacks|
 |[06](https://adventofcode.com/2022/day/6)|unique|77805|76383|[python](python/06.py)|--|--|datastream|
 |[07](https://adventofcode.com/2022/day/7)|travesal|59140|57130|[python](python/07.py)|--|--|directories|
-|[08](https://adventofcode.com/2022/day/8)|--|--|--|--|--|--|--|
+|[08](https://adventofcode.com/2022/day/8)|matrix|42819|36319|[python](python/08.py)|--|--|tree heights|
 |[09](https://adventofcode.com/2022/day/9)|--|--|--|--|--|--|--|
 |[10](https://adventofcode.com/2022/day/10)|--|--|--|--|--|--|--|
 |[11](https://adventofcode.com/2022/day/11)|--|--|--|--|--|--|--|
---
+++ python/08.py
@@ -0,0 +1,80 @@
+trees = []
+with open('python/08.in','r') as f:
+    for x in f.readlines():
+        trees.append([int(tree) for tree in x.strip()])
+
+visible_trees = 2 * (len(trees) + len(trees[0]) - 2)
+
+for r in range(1, len(trees)-1):
+    for c in range(1, len(trees[r])-1):
+        
+        # tallest to the left
+        if trees[r][c] > max(trees[r][:c]):
+            visible_trees += 1
+            continue
+        
+        # tallest to the right
+        if trees[r][c] > max(trees[r][c+1:]):
+            visible_trees += 1
+            continue
+        
+        # tallest up
+        visible_up = True
+        for t_r in range(r):
+            if trees[t_r][c] >= trees[r][c]:
+                visible_up = False
+                break
+
+        if visible_up:
+            visible_trees += 1
+            continue
+
+        visible_down = True
+        for t_r in range(r+1,len(trees)):
+            if trees[t_r][c] >= trees[r][c]:
+                visible_down = False
+                break
+        
+        visible_trees += visible_down
+
+print(visible_trees)
+#1487 too low
+
+best_scenic_score = 0
+
+# ignore edges, their scenic scores are 0
+for r in range(1, len(trees)-1):
+    for c in range(1, len(trees[r])-1):
+        #up
+        scenic_up = 0
+        for t_r in range(r-1, -1, -1):
+            scenic_up += 1
+            if trees[t_r][c] >= trees[r][c]:
+                break
+
+        #down
+        scenic_down = 0
+        for t_r in range(r+1, len(trees)):
+            scenic_down += 1
+            if trees[t_r][c] >= trees[r][c]:
+                break
+            
+        #right
+        scenic_right = 0
+        for t_c in range(c+1, len(trees[r])):
+            scenic_right += 1
+            if trees[r][t_c] >= trees[r][c]:
+                break
+            
+        #left
+        scenic_left = 0
+        for t_c in range(c-1, -1, -1):
+            scenic_left += 1
+            if trees[r][t_c] >= trees[r][c]:
+                break
+            
+        scenic_score = scenic_up * scenic_down * scenic_left * scenic_right
+        best_scenic_score = max(best_scenic_score, scenic_score)
+
+print(best_scenic_score)
+# not 470400 (too low)
