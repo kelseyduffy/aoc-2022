118f24ce40abbf87e0fd8a112cb8eec8108038c9
kelseyduffy <kduffy@osisoft.com>
2022-12-11 11:27:19 -0500

    python day 11 part 2

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ Whatever language I feel like that day. Probably mostly Python and C, maybe some
 |[08](https://adventofcode.com/2022/day/8)|matrix|42819|36319|[python](python/08.py)|--|--|tree heights|
 |[09](https://adventofcode.com/2022/day/9)|matrix|4707|30678|[python](python/09.py)|--|--|rope|
 |[10](https://adventofcode.com/2022/day/10)|increments|36474|33310|[python](python/10.py)|--|--|clock cycles|
-|[11](https://adventofcode.com/2022/day/11)|queues|--|--|--|--|--|monkey business|
+|[11](https://adventofcode.com/2022/day/11)|queues|32518|24355|[python](python/day11.py)|--|--|monkey business|
 |[12](https://adventofcode.com/2022/day/12)|--|--|--|--|--|--|--|
 |[13](https://adventofcode.com/2022/day/13)|--|--|--|--|--|--|--|
 |[14](https://adventofcode.com/2022/day/14)|--|--|--|--|--|--|--|
--- python/11.py
+++ python/11.py
@@ -1,6 +1,5 @@
-import math
-import queue
 from enum import Enum
+from math import gcd
 
 class math_op(Enum):
     ADD = 1
@@ -16,83 +15,100 @@ class Monkey:
         self.fail_monkey = fail_monkey
         self.inspection_count = 0
 
-monkeys = []
-items = []
-op = math_op.ADD
-op_num = 0
-test_num = 0
-pass_monkey = 0
-fail_monkey = 0
-with open('python/11.in','r') as f:
-    for x in f.readlines():
-        x = x.strip()
-        if x.startswith('Monkey'):
-            pass
-        elif x.startswith('Starting'):
-            parts = x.split(':')
-            parts[1].replace(' ','')
-            nums = parts[1].split(',')
-            items = [int(num) for num in nums]
-        elif x.startswith('Operation'):
-            parts = x.split(':')
-            terms = parts[1].split(' ')
-            if terms[-2] == '*':
-                op = math_op.MULT
-            elif terms[-2] == '+':
+def part1_worry_adjustor(worry):
+    return worry // 3
+
+def part2_worry_adjustor(worry):
+    global lcm
+    return worry % lcm
+
+counts = [20, 10000]
+worry_adjustors = [part1_worry_adjustor, part2_worry_adjustor]
+
+for i in range(len(counts)):
+
+    monkeys = []
+    items = []
+    op = math_op.ADD
+    op_num = 0
+    test_num = 0
+    pass_monkey = 0
+    fail_monkey = 0
+    with open('python/11.in','r') as f:
+        for x in f.readlines():
+            x = x.strip()
+            if x.startswith('Monkey'):
+                pass
+            elif x.startswith('Starting'):
+                parts = x.split(':')
+                parts[1].replace(' ','')
+                nums = parts[1].split(',')
+                items = [int(num) for num in nums]
+            elif x.startswith('Operation'):
+                parts = x.split(':')
+                terms = parts[1].split(' ')
+                if terms[-2] == '*':
+                    op = math_op.MULT
+                elif terms[-2] == '+':
+                    op = math_op.ADD
+                if terms[-1] == 'old':
+                    op_num = -1
+                else:
+                    op_num = int(terms[-1])
+            elif x.startswith('Test'):
+                parts = x.split('divisible by ')
+                test_num = int(parts[1])
+            elif x.startswith('If true'):
+                parts = x.split(' monkey ')
+                pass_monkey = int(parts[1])
+            elif x.startswith('If false'):
+                parts = x.split(' monkey ')
+                fail_monkey = int(parts[1])
+            elif x == '':
+                monkeys.append(Monkey(items, op, op_num, test_num, pass_monkey, fail_monkey))
+                items = []
                 op = math_op.ADD
-            if terms[-1] == 'old':
-                op_num = -1
-            else:
-                op_num = int(terms[-1])
-        elif x.startswith('Test'):
-            parts = x.split('divisible by ')
-            test_num = int(parts[1])
-        elif x.startswith('If true'):
-            parts = x.split(' monkey ')
-            pass_monkey = int(parts[1])
-        elif x.startswith('If false'):
-            parts = x.split(' monkey ')
-            fail_monkey = int(parts[1])
-        elif x == '':
-            monkeys.append(Monkey(items, op, op_num, test_num, pass_monkey, fail_monkey))
-            items = []
-            op = math_op.ADD
-            op_num = 0
-            test_num = 0
-            pass_monkey = 0
-            fail_monkey = 0
-    monkeys.append(Monkey(items, op, op_num, test_num, pass_monkey, fail_monkey))
+                op_num = 0
+                test_num = 0
+                pass_monkey = 0
+                fail_monkey = 0
+        monkeys.append(Monkey(items, op, op_num, test_num, pass_monkey, fail_monkey))
 
-for _ in range(20):
-    for monkey in monkeys:
-        while(len(monkey.items) > 0):
+    all_divisors = [monkey.test_num for monkey in monkeys]
 
-            worry_score = monkey.items.pop(0)
-            monkey.inspection_count += 1
-
-            term = monkey.op_num
-            if term == -1:
-                term = worry_score
-
-            if monkey.op == math_op.ADD:
-                worry_score += term
-            else:
-                worry_score *= term
-
-            worry_score //= 3
-
-            if worry_score % monkey.test_num == 0:
-                monkeys[monkey.pass_monkey].items.append(worry_score)
-            else:
-                monkeys[monkey.fail_monkey].items.append(worry_score)
-
-inspection_counts = []
-for monkey in monkeys:
-    inspection_counts.append(monkey.inspection_count)
-
-inspection_counts.sort()
-print(inspection_counts[-1] * inspection_counts[-2])
+    lcm = 1
+    for divisor in all_divisors:
+        lcm = lcm * divisor
 
 
+    for _ in range(counts[i]):
+        for monkey in monkeys:
+            while(len(monkey.items) > 0):
+
+                worry_score = monkey.items.pop(0)
+                monkey.inspection_count += 1
+
+                term = monkey.op_num
+                if term == -1:
+                    term = worry_score
+
+                if monkey.op == math_op.ADD:
+                    worry_score += term
+                else:
+                    worry_score *= term
+
+                worry_score = worry_adjustors[i](worry_score)
+
+                if worry_score % monkey.test_num == 0:
+                    monkeys[monkey.pass_monkey].items.append(worry_score)
+                else:
+                    monkeys[monkey.fail_monkey].items.append(worry_score)
+
+    inspection_counts = [monkey.inspection_count for monkey in monkeys]
+
+    inspection_counts.sort()
+    print(inspection_counts[-1] * inspection_counts[-2])
+
+# too high: 14400960000
 
 
